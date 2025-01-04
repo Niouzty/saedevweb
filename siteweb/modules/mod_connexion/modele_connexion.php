efe3c447e84a108e4c36478d0c83e3e7cbf3ce48
Niouzty <164004719+Niouzty@users.noreply.github.com>
2025-01-04 23:50:49 +0100

    modification modele_connexion.php( connexion possible pour les etudiants et responsables)

--- siteweb/modules/mod_connexion/modele_connexion.php
+++ siteweb/modules/mod_connexion/modele_connexion.php
@@ -1,17 +1,29 @@
 <?php
 class ModeleConnexion extends ModeleGenerique {
-    public function verifierUtilisateur($username, $password) {
-        $query = $this->bdd->prepare("SELECT * FROM users WHERE username = :username");
-        $query->bindParam(':username', $username);
+    public function verifierEnseignant($email, $password) {
+        $query = $this->bdd->prepare("SELECT * FROM enseignant WHERE email = :email");
+        $query->bindParam(':email', $email);
         $query->execute();
 
-        $user = $query->fetch();
-        if ($user && password_verify($password, $user['password'])) {
-            return $user;
+        $enseignant = $query->fetch();
+        if ($enseignant && password_verify($password, $enseignant['password'])) {
+            return $enseignant;
+        }
+
+        return false;
+    }
+
+    public function verifierEtudiant($email, $password) {
+        $query = $this->bdd->prepare("SELECT * FROM etudiant WHERE email = :email");
+        $query->bindParam(':email', $email);
+        $query->execute();
+
+        $etudiant = $query->fetch();
+        if ($etudiant && password_verify($password, $etudiant['password'])) {
+            return $etudiant;
         }
 
         return false;
     }
 }
 ?>
-
