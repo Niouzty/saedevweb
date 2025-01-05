4310e5d7979bec303b568f3bb1b371c68ef04595
Niouzty <164004719+Niouzty@users.noreply.github.com>
2025-01-05 03:33:10 +0100

    Update vue_connexion.php

--- siteweb/modules/mod_connexion/vue_connexion.php
+++ siteweb/modules/mod_connexion/vue_connexion.php
@@ -17,7 +17,7 @@ class VueConnexion extends VueGenerique {
                 <div class="login-form">
                     <h1>Connexion</h1>
                     <form action="?module=connexion&action=verifier" method="POST">
-                        <label for="email">Email (Email):</label>
+                        <label for="email">Identifiant (Email):</label>
                         <input type="email" id="email" name="email" required>
                         
                         <label for="password">Mot de passe:</label>
