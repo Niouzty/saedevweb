d66cc00b1ecd1d070d003165db84cb029293138d
Niouzty <164004719+Niouzty@users.noreply.github.com>
2025-01-06 23:10:01 +0100

    ajout du lien vers public/css

--- siteweb/modules/mod_utilisateur/vue_utilisateur-enseignant.php
+++ siteweb/modules/mod_utilisateur/vue_utilisateur-enseignant.php
@@ -9,7 +9,7 @@ class VueUtilisateur extends VueGenerique {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accueil Étudiant</title>
-    <link rel="stylesheet" href="style-etudiant.css"> <!-- Lien vers le fichier CSS -->
+    <link rel="stylesheet" href="/public/css/style.css"> <!-- Lien vers le CSS -->
 </head>
 <body>
     <!-- Barre de navigation -->
