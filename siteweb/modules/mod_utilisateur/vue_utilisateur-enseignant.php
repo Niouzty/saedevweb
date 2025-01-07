5ba20bd8af9212bf84e0a939cf19ed11ae101f94
Niouzty <164004719+Niouzty@users.noreply.github.com>
2025-01-07 01:29:59 +0100

    Update vue_utilisateur-enseignant.php

--- siteweb/modules/mod_utilisateur/vue_utilisateur-enseignant.php
+++ siteweb/modules/mod_utilisateur/vue_utilisateur-enseignant.php
@@ -11,7 +11,7 @@ error_reporting(E_ALL);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Accueil Étudiant</title>
+    <title>Accueil Enseignant</title>
     <link rel="stylesheet" href="/public/css/style.css"> <!-- Lien vers le CSS -->
 </head>
 <body>
