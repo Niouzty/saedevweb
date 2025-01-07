b2f384d6b8762fb848f4f53841d8f854823ecc91
Niouzty <epembelefuala@iut.univ-paris8.fr>
2025-01-07 23:36:11 +0100

    Ajout vue messagerie

---
+++ siteweb/modules/mod_messagerie/vue_messagerie.php
@@ -0,0 +1,36 @@
+<?php
+class VueMessagerie extends VueGenerique {
+    // Afficher la liste des groupes
+    public function afficherGroupes($groupes) {
+        echo '<div class="group-list">';
+        foreach ($groupes as $groupe) {
+            echo "<div class='group-item' data-id='{$groupe['id']}'>{$groupe['name']}</div>";
+        }
+        echo '</div>';
+    }
+
+    // Afficher les messages d'un groupe
+    public function afficherMessages($messages) {
+        echo '<div class="message-body">';
+        foreach ($messages as $message) {
+            echo "<div class='message'>
+                    <strong>{$message['name']}:</strong> {$message['content']}
+                    <span>{$message['created_at']}</span>
+                  </div>";
+        }
+        echo '</div>';
+    }
+
+    // Afficher le champ pour envoyer un message
+    public function afficherFormulaireMessage($groupId) {
+        echo "
+        <div class='message-input'>
+            <form action='?module=messagerie&action=envoyer' method='POST'>
+                <input type='hidden' name='group_id' value='{$groupId}'>
+                <input type='text' name='content' placeholder='Écrire un message' required>
+                <button type='submit'>Envoyer</button>
+            </form>
+        </div>";
+    }
+}
+
