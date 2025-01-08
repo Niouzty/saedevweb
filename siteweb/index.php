0d9dad3d238be520241b3d1005f79c97776cebe3
Niouzty <164004719+Niouzty@users.noreply.github.com>
2025-01-08 13:14:57 +0100

    Update index.php

--- siteweb/index.php
+++ siteweb/index.php
@@ -4,6 +4,10 @@ ini_set('display_errors', 1);
 
 require_once "connexion.php";
 require_once "site.php";
+  require_once __DIR__ . '/core/module_generique.php';
+    require_once __DIR__ . '/core/modele_generique.php';
+    require_once __DIR__ . '/core/vue_generique.php';
+    require_once __DIR__ . '/templates/template.php';
 
 //Connexion::init_connexion();
 
